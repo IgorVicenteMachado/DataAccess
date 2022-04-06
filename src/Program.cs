1bbb52e4bbaa3778541bf7e33a3a9fdcae0e2de3
IgorVicente <igor_machado05@hotmail.com>
2022-04-06 17:29:24 -0300

    const string de conexao

--- src/Program.cs
+++ src/Program.cs
@@ -2,8 +2,12 @@ namespace dataaccess
 {
     class Program
     {
+        private const string CONNECTION_STRING = 
+            @"Server = localhost,1433; Database = balta; User ID = sa; password =@temp;TrustServerCertificate=True";
+            
         static void Main(string[] args)
         {
+
             Console.WriteLine("hello, world");
         }
     }
