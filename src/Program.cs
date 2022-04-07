799d064c4b61c17ea211fa0c30b118df75fe6e35
IgorVicente <igor_machado05@hotmail.com>
2022-04-07 11:39:54 -0300

    ADO puro: recuperando dados do banco

--- src/Program.cs
+++ src/Program.cs
@@ -4,13 +4,27 @@ namespace dataaccess
     class Program
     {
         private const string CONNECTION_STRING = 
-            @"Server = localhost,1433; Database = balta; User ID = sa; password =@temp;TrustServerCertificate=True";
+            @"Server = localhost,1433; Database=praticandodb; User ID=sa; password=!@#$%12345;TrustServerCertificate=True";
             
         static void Main(string[] args)
         {
-            using (var connection = new SqlConnection())
+             using (var connection = new SqlConnection(CONNECTION_STRING))
             {
-                //acesso ao banco de dados
+                Console.WriteLine("Conectado");
+                connection.Open();
+
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                    }
+                }
             }
         }
     }
